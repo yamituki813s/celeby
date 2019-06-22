cfe568323da981a3ddf6a84bd98b481938f0f97f
skycat <mt@skycat.me>
2019-06-23 00:35:41 +0900

    add Firestoreから取得したデータをスライドさせる

---
+++ imageviewer/src/components/ImageViewer.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <div v-if="showImageurl">
+      <div
+        id="viewer-area"
+        :style="{ backgroundImage: 'url(' + showImageurl + ')' }"
+      ></div>
+    </div>
+    <div v-else>
+      準備中。。。。
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { db } from "../plugins/firebase";
+
+const images = db.collection("images");
+
+@Component
+export default class ImageViewer extends Vue {
+  images: firebase.firestore.DocumentData = [];
+  swiperOption = {
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 50,
+    loop: true
+  };
+  timer: number = 0;
+  index: number = 0;
+  showImageurl: string | null = null;
+  changeTime: number = 5000;
+
+  created() {
+    db.collection("image").onSnapshot(ss => {
+      clearInterval(this.timer);
+      let w: firebase.firestore.DocumentData = [];
+      ss.forEach(doc => w.push(doc.data()));
+      this.images = w;
+      this.showImageurl = this.images[0]["url"] || null;
+      this.onTimer();
+    });
+  }
+  onTimer() {
+    this.timer = setInterval(() => {
+      const nextIndex: number =
+        this.images.length > this.index + 1 ? this.index + 1 : 0;
+      this.showImageurl = this.images[nextIndex]["url"];
+      this.index = nextIndex;
+    }, this.changeTime);
+  }
+  mounted() {
+    this.onTimer();
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#viewer-area {
+  position: relative;
+  overflow: hidden;
+  width: 100%;
+  height: 100vh;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-attachment: fixed;
+}
+</style>
--- imageviewer/src/views/Home.vue
+++ imageviewer/src/views/Home.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <ImageViewer />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import ImageViewer from "@/components/ImageViewer.vue"; // @ is an alias to /src
 
 @Component({
   components: {
-    HelloWorld
+    ImageViewer
   }
 })
 export default class Home extends Vue {}
